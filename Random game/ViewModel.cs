2bc1ad1352ec157068d419da90f6b06eae460d05
fastikif <fastikif@gmail.com>
2026-02-08 18:24:02 +0300

    Remove button Update

--- Random game/ViewModel.cs
+++ Random game/ViewModel.cs
@@ -7,12 +7,19 @@ public class ViewModel : INotifyPropertyChanged
 {
     private Random _random = new Random();
     public ICommand RerollCommand { get;}
-    public ICommand ShowStatsCommand { get;}
     public ICommand FightCommand {get;}
     public ModelStats _modelStats;
     private string _statsResult;
     private string _enemyStatsResult;
     private string _winnerResult;
+
+    private void ShowStats()
+    {
+        StatsResult = $"damage:{_modelStats.Damage} health:{_modelStats.Health}";
+
+        EnemyStatsResult = $"damage:{_modelStats.Damage1} health:{_modelStats.Health1}";
+    }
+
     public string WinnerResult
     {
         get => _winnerResult;
@@ -54,43 +61,38 @@ public class ViewModel : INotifyPropertyChanged
          _modelStats.Health = Randoming(200, 500);
          _modelStats.Damage = Randoming(50, 100);
          _modelStats.Damage1 = Randoming(50, 100);
+         ShowStats();
     }
 
     public void Fight()
     {
+        
         while (_modelStats.Health1 >= 0 && _modelStats.Health >= 0)
         {
             _modelStats.Health1 = _modelStats.Health1 - _modelStats.Damage;
+            ShowStats();
             break;
         }
 
         while (_modelStats.Health1 >= 0 && _modelStats.Health >= 0)
         {
             _modelStats.Health = _modelStats.Health - _modelStats.Damage1;
+            ShowStats();
             return;
         }
-
+        
         if (_modelStats.Health <= 0)
             WinnerResult =  "Winner: You!";
         else if (_modelStats.Health1 <= 0)
             WinnerResult = "Winner: Enemy!";
+        
     }
 
     public ViewModel()
     {
         _modelStats = new ModelStats();
-        ShowStatsCommand = new RelayCommand
-        (
-            execute: _ =>
-            {
-                StatsResult = $"damage:{_modelStats.Damage} health:{_modelStats.Health}";
-
-                EnemyStatsResult = $"damage:{_modelStats.Damage1} health:{_modelStats.Health1}";
-            },
-        canExecute: _=> true );
-        
         RerollCommand = new RelayCommand(execute: _ => RandomReroll(), canExecute: _=> true);
-        FightCommand = new RelayCommand(execute :_ => Fight(),canExecute: _=> true);
+        FightCommand = new RelayCommand(execute: _ => Fight(), canExecute: _=> true);
     }
    
     public event PropertyChangedEventHandler PropertyChanged;
